5f5bd2a218830c31a68d91eaf1f372822f8c0d4e
ashrithbalgury3824 <2320030442@klh.edu.in>
2024-08-06 13:28:12 +0530

    Add files via upload

---
+++ LAB/src/lab1b/Main1b.java
@@ -0,0 +1,9 @@
+package lab1b;
+
+public class Main1b {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Banker banker = Banker.getInstance();
+		banker.log("1,05,033", "1000", "50");
+	}
+}
