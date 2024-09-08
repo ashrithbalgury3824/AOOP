19e8b10dabdaee50563cd5d742b7744ac5d7cc9f
ashrithbalgury3824 <2320030442@klh.edu.in>
2024-09-08 23:12:18 +0530

    Delete LAB6-Practical directory

--- LAB6-Practical/lab6(i)/src/lab6_i/Main.java
+++
@@ -1,8 +0,0 @@
-package lab6_i;
-
-public class Main {
-    public static void main(String[] args) {
-        TaskManagementSystem system = new TaskManagementSystem();
-        system.run();
-    }
-}
